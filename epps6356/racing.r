3605983b3a0edcc4b4c30cbf692a8edaddacdadf
Guanhc416 <121653734+GuanHChen@users.noreply.github.com>
2025-10-01 19:50:29 -0500

    boxplot racing

---
+++ epps6356/racing.r
@@ -0,0 +1,11 @@
+library(readxl)
+library(dplyr)
+df <- read_excel('epps6356/myHPI.xlsx')
+df <- na.omit(df)
+
+boxplot(df$GDP,
+        main = "Boxplot of GDP",  # Add a title
+        ylab = "GDP Values",      # Label the y-axis
+        col = "lightblue",        # Set the box color
+        border = "black"          # Set the border color
+)
